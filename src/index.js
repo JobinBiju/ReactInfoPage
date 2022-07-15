d60c486332a0b52143290fc43d68f7b49e6da057
Jobin Biju <jobinbiju9090@gmail.com>
2022-07-15 22:55:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./assets/images/react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+function Page() {
+  return (
+    <>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first introduced in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Jobin development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+ReactDOM.render(
+  <>
+    <Header />
+    <Page />
+    <Footer />
+  </>,
+  document.getElementById('root'),
+);
+
