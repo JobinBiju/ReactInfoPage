3a517adfb92fd8652f94131ac6f9c56c773818e8
Jobin Biju <jobinbiju9090@gmail.com>
2022-07-25 18:26:03 +0530

    Learned state

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../assets/images/react-logo.png';
 
 function MainContent() {
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="main">
       <h1>Fun facts about React</h1>
-      <ul className='mainList'>
+      <ul className="mainList">
         <li>Was first introduced in 2013</li>
         <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
+        <li>Has well over {count}K stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Power thousands of enterprise apps, including mobile apps</li>
       </ul>
+      <button className="count-btn" onClick={incrementCount}>
+        <img src={logo} className="logo-bg" />
+      </button>
     </div>
   );
 }
